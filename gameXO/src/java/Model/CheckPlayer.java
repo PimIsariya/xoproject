06df1448b002a384b39f73657db08fc4735fb7a3
Wipapun <wipapun.tec07@mail.kmutt.ac.th>
2018-08-04 14:48:36 +0700

    Checkplayer class

---
+++ gameXO/src/java/Model/CheckPlayer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author WIN10_M7
+ */
+public class CheckPlayer {
+    public  String checkPlayer(int count){
+        String player;
+        if(count % 2 == 0){
+            return player = "P1";
+        }else{
+            return player = "P2";
+        }
+        
+       
+    }
+}
