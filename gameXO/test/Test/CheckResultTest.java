c7d7a76af66dd29f21c894640053eb6cf30ae29e
korkeng <korkeng@hotmail.com>
2018-08-04 14:28:22 +0700

    add more

--- gameXO/test/Test/CheckResultTest.java
+++ gameXO/test/Test/CheckResultTest.java
@@ -1,26 +1,19 @@
 package Test;
 
 
-import Model.CheckHorizontal;
+import Model.CheckResult;
 import org.junit.Assert;
 import org.junit.Test;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author User
- */
-public class HorizontalTest {
+
+public class CheckResultTest {
 
     @Test
-    public void checkPattenHorizontalWin() {
-        CheckHorizontal c = new CheckHorizontal();
-        String pattenHorizontalWin[] = {"o", "o", "o", "", "x", "", "x", "", ""};
-        c.setArrayHorizontal(pattenHorizontalWin);
-        Assert.assertTrue(c.getCheckBoolean());
+    public void checkAllResultWin() {
+        CheckResult cr = new CheckResult();
+        cr.setDiagonalToWin(true);
+        cr.setHorizontalToWin(false);
+        cr.setVerticalToWin(false);
+        Assert.assertSame(cr.isWinOrTie(), "Win");
     }
 }
